2922a857c99c230218766b49919e63e7fb3c580c
SBal <saubhagyabal2001@gmail.com>
2023-02-24 10:48:21 +0530

    card-2

--- src/cred/OptionCard.js
+++ src/cred/OptionCard.js
@@ -1,29 +1,27 @@
 import "./OptionCard.css";
 import { Button, Typography, Column } from "@cred/neopop-web/lib/components";
 import { FontVariant } from "@cred/neopop-web/lib/primitives";
-
+import image1 from "../images/party.png";
 function Card(props) {
   return (
     <div className="card-main">
-      <Column>
-        <div>dfnwuehuifbw</div>
-        <div className="option-name">
-          <div className="heading">
-            <p>{props.name}</p>
-          </div>
-          <div className="view-button">
-            <Button
-              variant="secondary"
-              kind="elevated"
-              size="medijm"
-              showArrow
-              fullWidth
-            >
-              View
-            </Button>
-          </div>
+      <div className="option-image">
+        <img src={image1} style={{ width: "160px", height: "60%" }} />
+      </div>
+      <div className="option-name">
+        <div className="option-heading">{props.name}</div>
+        <div className="view-button">
+          <Button
+            variant="secondary"
+            kind="elevated"
+            size="medijm"
+            showArrow
+            fullWidth
+          >
+            View
+          </Button>
         </div>
-      </Column>
+      </div>
     </div>
   );
 }
