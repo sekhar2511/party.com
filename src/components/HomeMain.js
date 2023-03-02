57850b59d1c697989b275ecc0cc3856c5c36b452
SBal <saubhagyabal2001@gmail.com>
2023-03-02 16:19:23 +0530

    ds

--- src/components/HomeMain.js
+++ src/components/HomeMain.js
@@ -13,9 +13,7 @@ function HomeMain() {
         <div>
           <Card name="birthday" />
         </div>
-        <div>
-          <Card name="concert" />
-        </div>
+
         <div>
           <Card name="credle ceremony" />
         </div>
