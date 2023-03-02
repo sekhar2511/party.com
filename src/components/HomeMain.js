c3238a7b1fdf57283b6d67e9d1f7d9b17ec58b00
SBal <saubhagyabal2001@gmail.com>
2023-03-02 16:14:41 +0530

    d

--- src/components/HomeMain.js
+++ src/components/HomeMain.js
@@ -1,13 +1,21 @@
 import Card from "../cred/OptionCard";
 import "./HomeMain.css";
+import Ads from "../cred/Ads";
+import { Column } from "@cred/neopop-web/lib/components";
+
 function HomeMain() {
   return (
     <div>
+      <div className="ads-card">
+        <Ads />
+      </div>
       <div className="flex-home">
         <div>
           <Card name="birthday" />
         </div>
-
+        <div>
+          <Card name="concert" />
+        </div>
         <div>
           <Card name="credle ceremony" />
         </div>
