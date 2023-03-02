c91048a81b867f1dcf8e7ff501145dc80d07923c
HAPPY <cse.190310218@silicon.ac.in>
2023-03-02 15:01:26 +0530

    df

--- src/components/HomeMain.js
+++ src/components/HomeMain.js
@@ -1,8 +1,14 @@
 import Card from "../cred/OptionCard";
 import "./HomeMain.css";
+import Ads from "../cred/Ads";
+import { Column } from "@cred/neopop-web/lib/components";
+
 function HomeMain() {
   return (
     <div>
+      <div className="ads-card">
+        <Ads />
+      </div>
       <div className="flex-home">
         <div>
           <Card name="birthday" />
