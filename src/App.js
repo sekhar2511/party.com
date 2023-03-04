3937966a52f1a411aea84d9fdb30dae15f47e4d7
HAPPY <cse.190310218@silicon.ac.in>
2023-03-04 12:21:24 +0530

    added landing page

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import About from "./pages/about";
 import HomeMain from "./components/HomeMain";
+import Landing from "./pages/landing";
+
 function App() {
   return (
-    <div>
-      <Router>
+    <>
+      <p>working</p>
+      <Landing />
+      {/* <Router>
         <Navbar />
         <Routes>
           <Route path="/" element={<HomeMain />} exact />
           <Route path="/about" element={<About />} />
-          {/* <Route path='/services' component={Services} />
-        <Route path='/contact-us' component={Contact} />
-        <Route path='/sign-up' component={SignUp} /> */}
+          <Route path="/services" element={<HomeMain />} />
+          <Route path="/contact-us" element={<HomeMain />} />
+          <Route path="/sign-up" element={<HomeMain />} />
         </Routes>
-      </Router>
-    </div>
+      </Router> */}
+    </>
   );
 }
 
---
+++ src/pages/landing.js
@@ -0,0 +1,12 @@
+import "./landing.css";
+
+function Landing() {
+  return (
+    <div>
+      <div className="left">left</div>
+      <div className="right">right</div>
+    </div>
+  );
+}
+
+export default Landing;
